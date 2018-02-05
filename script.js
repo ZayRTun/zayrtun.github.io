1668d851ea3bd0a1e857d4f1f3425cb0f3bcb0c0
ZayRTun <zay.t@hotmail.com>
2018-02-05 13:07:06 +0630

    smooth scroll updated

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 $(function() {
-  $("a[href^='#']").on("click", function(event) {
-    event.preventDefault();
+  // $("a[href^='#']").on("click", function(event) {
+  //   event.preventDefault();
     
-    var target = this.hash;
-    var $target = $(target);
+  //   var target = this.hash;
+  //   var $target = $(target);
 
 
-    $('html, body').animate({
-      'scrollTop': $target.offset().top
-      }, 1000, 'swing', function () {
-        window.location.hash = target;
-      });
+  //   $('html, body').animate({
+  //     'scrollTop': $target.offset().top
+  //     }, 1000, 'swing', function () {
+  //       window.location.hash = target;
+  //     });
+  // })
+
+  var scrollLink = $(".scLinks");
+  
+  scrollLink.click(function(event) {
+    event.preventDefault();
+    var hashT = $(this.hash);
+    var loc = hashT[0].id;
+
+    $("body,html").animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000, "swing", function() {
+      window.location.hash = loc;
+    })
   })
-});
+
+  $(window).scroll(function() {
+    var scBarLoc = $(this).scrollTop();
+    scrollLink.each(function() {
+      var secOffSet = $(this.hash).offset().top - 20;
+
+      if (secOffSet <= scBarLoc) {
+        $(this).addClass("active");
+        $(this).parent().siblings().children().removeClass("active");
+      }
+    })
+  })
+  
+})
