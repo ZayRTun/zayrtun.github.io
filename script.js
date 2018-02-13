1934e76a6b25bf9fe7412fe5f193c7dabf80d621
ZayRTun <zay.t@hotmail.com>
2018-02-13 16:01:38 +0630

    AboutMe Updated

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 $(function() {
-  // $("a[href^='#']").on("click", function(event) {
-  //   event.preventDefault();
-    
-  //   var target = this.hash;
-  //   var $target = $(target);
-
-
-  //   $('html, body').animate({
-  //     'scrollTop': $target.offset().top
-  //     }, 1000, 'swing', function () {
-  //       window.location.hash = target;
-  //     });
-  // })
 
   var scrollLink = $(".scLinks");
   
